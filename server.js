44383d68ce89297514221d87745d228eb31ce976
Nathan Gaulard <gaulardnathan@gmail.com>
2023-05-02 19:05:19 +0200

    create login

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const url = require('url');
 const fs = require('fs');
 const ejs = require('ejs');
 const app = express();
-
-
 const server = http.createServer();
+
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 server.listen(8080, 'localhost');  
 server.on('listening', () => console.log('Serveur démarré !'));     
 
+// Système de gestion url
 server.on('request', function(request, response) {
     var page = url.parse(request.url).pathname ;
     response.writeHead(200,{"Content-Type": "text/html; charset=UTF-8"}) ;
@@ -30,8 +32,25 @@ server.on('request', function(request, response) {
               console.log(error);
               response.end(content);
             })
-            
             break ;
+        case '/chat' : 
+            ejs.renderFile(__dirname + "\\socket.ejs", {title: 'Chat'}, (error, content) => {
+              console.log(error);
+              response.end(content);
+            })
+            break ;
+        case '/login' : 
+            ejs.renderFile(__dirname + "\\login.html", {title: 'Login'}, (error, content) => {
+              console.log(error);
+              response.end(content);
+            })
+            break ;
+        // case '/chat2' : 
+        //     res.renderFile(__dirname + "\\chat.html", {title: 'Chat'}, (error, content) => {
+        //       console.log(error);
+        //       response.end(content);
+        //     })
+        //     break ;
         case '/error404' : 
             ejs.renderFile(__dirname + "\\Error404.ejs", {title: 'Error404'}, (error, content) => {
               console.log(error);
@@ -48,3 +67,14 @@ server.on('request', function(request, response) {
         }
     }
 });
+
+// // Système de chat
+// io.on('connection', (socket) => {
+//   console.log('user connected');
+//   socket.on('chat message', (msg) => {
+//     io.emit('chat message', msg);
+//   });
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
